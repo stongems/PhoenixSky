63df4f9cb7b0340d2eee6f305e13fa5f06a06146
Zachary K <zachwkrause@gmail.com>
2021-06-12 02:25:02 -0700

    HTML base structure added, Unsplash API functiionality Updated, and JS organized and updated.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,60 @@
 const openNavBarBtn = document.querySelector("#openNavBarBtn");
 const closeNavBarBtn = document.querySelector("#closeNavBarBtn");
 const navBarMenu = document.querySelector("#navBarMenu");
-const p1SubBtn = document.querySelector("#p1SubBtn"); 
+const p1SubBtn = document.querySelector("#p1SubBtn");
+const destinationID = document.querySelector("#destinationID");
 
-function createFlightHtml() {
-  //create HTML elements here
-}
+// USER TEXT INPUT SAVE AND SUBMIT BUTTON
+p1SubBtn.addEventListener("click", function (e) 
+{
+  e.preventDefault();
+  if (destinationID.value == "") 
+  {
+    console.log("I cant do that Dave");
+  } 
+  else if (destinationID.value !== "") 
+  {
+    localStorage.setItem("UserDestination", JSON.stringify(destinationID.value));
+    //FUNCTION TRIGGERS FOR API, WILL START ON BUTTON CLICK. USE>>> localStorage.getItem("UserDestination") <<<TO PULL USER INPUT TEXT.
+    runOpenWeatherAPI();
+    runAbstractAPI();
+    runAmadeusAPI();
+    runUnspashAPI();
+    window.location.reload(); //<<<<<<<<<<<<<<<<<!!!!PAGE REFRESHES HERE ON SUBMIT!!!! if something is broken try removing this
+  };
+});
+// function submitDestination() {let UserDestination = localStorage.getItem("UserDestination");}
 
+
+
+
+// EMPTY FUNCTIONS FOR API OUTPUTS
+function runOpenWeatherAPI() {
+  
+};
+
+function runAbstractAPI() {
+  
+};
+
+function runAmadeusAPI() {
+  
+};
+
+function runUnspashAPI() {
+  localStorage.getItem("UserDestination")
+  console.log("W-LS: ",phoenixSkyBG)
+};
+// function createFlightHtml() {
+//   create HTML elements here
+// }
+
+
+
+
+
+
+// NAV BAR BUTTON LISTENERS 
 openNavBarBtn.addEventListener("click", function (e) {
   e.preventDefault();
   openNavBarBtn.classList.add("hidden");
