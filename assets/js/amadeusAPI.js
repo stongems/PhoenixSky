49d91c30eae8f982e7e89dfc1534a3ff8bb47a57
St.Onge <stonge.ms@gmail.com>
2021-06-13 16:07:04 -0700

    Huge update and fix to the UI HTML and CSS.

--- assets/js/amadeusAPI.js
+++ assets/js/amadeusAPI.js
@@ -1,10 +1,9 @@
-let destinationLocationCode = "LAS"
+let destinationLocationCode = "LAS";
 let departureDate = "2021-06-30";
 let destLat;
 let destLong;
 
-
-function getAccessToken(){
+function getAccessToken() {
   fetch("https://test.api.amadeus.com/v1/security/oauth2/token", {
     body: "grant_type=client_credentials&client_id=2fdNuvibX2M6d60L6dMzYlpxkH1jV4wg&client_secret=wFgzffD956eN8Aau",
     headers: {
@@ -12,23 +11,27 @@ function getAccessToken(){
     },
     method: "POST",
   })
-  .then((res) => res.json())
-  .then((data) => {
-    localStorage.setItem("Access_Token" , data.access_token)
-    
-  });
-  
+    .then((res) => res.json())
+    .then((data) => {
+      localStorage.setItem("Access_Token", data.access_token);
+    });
 }
 
 async function getFlightOffers() {
-  getAccessToken()
- await getOriginLocationCode();
+  getAccessToken();
+  await getOriginLocationCode();
 
   let access_token = localStorage.getItem("Access_Token");
-  let originLocationCode = localStorage.getItem("originLocationCode")
-  
+  let originLocationCode = localStorage.getItem("originLocationCode");
+
   fetch(
-    `https://test.api.amadeus.com/v2/shopping/flight-offers?adults=1&currencyCode=USD&max=2&originLocationCode=`+originLocationCode+`&destinationLocationCode=`+destinationLocationCode+`&departureDate=`+departureDate+``,
+    `https://test.api.amadeus.com/v2/shopping/flight-offers?adults=1&currencyCode=USD&max=2&originLocationCode=` +
+      originLocationCode +
+      `&destinationLocationCode=` +
+      destinationLocationCode +
+      `&departureDate=` +
+      departureDate +
+      ``,
     {
       headers: {
         Authorization: "Bearer " + access_token,
@@ -41,15 +44,19 @@ async function getFlightOffers() {
 
       // createFlightHtml(flightData);
     });
-};
+}
 
 function getOriginLocationCode() {
-  getUserLatLong()
+  getUserLatLong();
   let access_token = localStorage.getItem("Access_Token");
   let userLat = localStorage.getItem("userLat");
   let userLong = localStorage.getItem("userLong");
   return fetch(
-    `https://test.api.amadeus.com/v1/reference-data/locations/airports?latitude=`+userLat+`&longitude=`+userLong+`&radius=500&page[limit]=1`,
+    `https://test.api.amadeus.com/v1/reference-data/locations/airports?latitude=` +
+      userLat +
+      `&longitude=` +
+      userLong +
+      `&radius=500&page[limit]=1`,
     {
       headers: {
         Authorization: "Bearer " + access_token,
@@ -58,16 +65,20 @@ function getOriginLocationCode() {
   )
     .then((response) => response.json())
     .then((data) => {
-      localStorage.setItem("originLocationCode",data.data[0].iataCode)
+      localStorage.setItem("originLocationCode", data.data[0].iataCode);
     });
-};
+}
 
 function getDestinationLocationCode(token) {
   getDestLongLat();
   destLat = localStorage.getItem("recentCities.lat");
   destLong = localStorage.getItem("recentCities.lon");
   fetch(
-    `https://test.api.amadeus.com/v1/reference-data/locations/airports?latitude=`+destLat+`&longitude=`+destLong+`&radius=500page[limit]=1`,
+    `https://test.api.amadeus.com/v1/reference-data/locations/airports?latitude=` +
+      destLat +
+      `&longitude=` +
+      destLong +
+      `&radius=500page[limit]=1`,
     {
       headers: {
         Authorization: "Bearer " + token,
@@ -80,8 +91,3 @@ function getDestinationLocationCode(token) {
       return originLocationCode;
     });
 }
-
-
-
-
-
